3ea43773f25d31aa83d938e217fe4ff41a423d23
Hao Tran <hao.tran@go1.com>
2019-06-03 17:59:58 +0700

    Remove controls as default value of video

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -2,19 +2,11 @@ import * as React from "react";
 import ReactPlayer, { ReactPlayerProps } from "react-player";
 
 export type Props = ReactPlayerProps & {
-  innerRef: string;
+  innerRef?: React.RefObject<any>;
 };
 
 const VideoPlayer: React.SFC<Props> = ({ innerRef, ...props }) => {
-  return (
-    <ReactPlayer
-      height="100%"
-      width="100%"
-      controls={true}
-      {...props}
-      ref={innerRef}
-    />
-  );
+  return <ReactPlayer height="100%" width="100%" {...props} ref={innerRef} />;
 };
 
 VideoPlayer.displayName = "VideoPlayer";
