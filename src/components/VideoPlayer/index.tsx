25f3e4e18e01db7288baf719523c0fc54781e250
Hao Tran <hao.tran@go1.com>
2019-06-18 17:33:04 +0700

    Support getting dimensions for video

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,14 +1,116 @@
 import * as React from "react";
 import ReactPlayer, { ReactPlayerProps } from "react-player";
+import safeInvoke from "../../utils/safeInvoke";
 
-export type Props = ReactPlayerProps & {
-  innerRef?: React.RefObject<any>;
-};
+interface Dimensions {
+  width: number;
+  height: number;
+}
 
-const VideoPlayer: React.SFC<Props> = ({ innerRef, ...props }) => {
-  return <ReactPlayer height="100%" width="100%" {...props} ref={innerRef} />;
-};
+export interface VideoPlayerProps extends ReactPlayerProps {
+  onPlayerLoaded?: (player: any | null) => any;
+  onVideoDimensions?: (data: Dimensions) => any;
+}
 
-VideoPlayer.displayName = "VideoPlayer";
+interface Base {
+  predicate: (url: string, player: any) => boolean;
+}
 
-export default VideoPlayer;
+interface Video extends Base {
+  type: "video";
+  player?: HTMLVideoElement;
+}
+
+// interface Youtube extends Base {
+//   type: "youtube";
+//   player?: object;
+// }
+
+interface Vimeo extends Base {
+  type: "vimeo";
+  player?: {
+    getVideoWidth: () => Promise<number>;
+    getVideoHeight: () => Promise<number>;
+  };
+}
+
+type VideoType = Video | Vimeo;
+
+const SupportedRatioVendors: VideoType[] = [
+  {
+    type: "vimeo",
+    predicate: url => /http(s)?:\/\/(www.)?vimeo/.test(url),
+  },
+  {
+    type: "video",
+    predicate: (url, player) => url && player instanceof HTMLVideoElement,
+  },
+  // {
+  //   type: "youtube",
+  //   predicate: url => /http(s)?:\/\/(www.)?youtube/.test(url),
+  // },
+];
+
+export class VideoPlayer extends React.Component<VideoPlayerProps> {
+  private videoRef = React.createRef<ReactPlayer>();
+
+  public render() {
+    const { onVideoDimensions, onPlayerLoaded, onReady, ...props } = this.props;
+
+    return (
+      <ReactPlayer
+        height="100%"
+        width="100%"
+        onReady={this.onReady}
+        ref={this.videoRef}
+        {...props}
+      />
+    );
+  }
+
+  private getVendorPlayer(url: string, player: any | null): VideoType | null {
+    for (const vendor of SupportedRatioVendors) {
+      if (vendor.predicate(url, player)) {
+        return {
+          ...vendor,
+          player,
+        };
+      }
+    }
+
+    return null;
+  }
+
+  private async getDimensions(video: VideoType): Promise<Dimensions> {
+    switch (video.type) {
+      case "vimeo":
+        return {
+          width: await video.player.getVideoWidth(),
+          height: await video.player.getVideoHeight(),
+        };
+
+      case "video":
+      default:
+        return {
+          width: video.player.videoWidth,
+          height: video.player.videoHeight,
+        };
+    }
+  }
+
+  private onReady = async () => {
+    const player = this.videoRef.current.getInternalPlayer();
+    const vendor = this.getVendorPlayer(this.props.url as string, player);
+
+    if (player) {
+      safeInvoke(this.props.onPlayerLoaded, player);
+    }
+
+    if (vendor && vendor.player) {
+      const dimensions = await this.getDimensions(vendor);
+      safeInvoke(this.props.onVideoDimensions, dimensions);
+    }
+
+    safeInvoke(this.props.onReady);
+  };
+}
--- stories/VideoPlayer.story.js
+++ stories/VideoPlayer.story.js
@@ -3,12 +3,15 @@ import {storiesOf} from '@storybook/react';
 import { VideoPlayer } from '../src';
 
 storiesOf('VideoPlayer', module)
-  .add('with specific URL', () => <div style={{ width: '50%' }}>
-      <VideoPlayer url="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" controls />
+  .add('with specific URL', () => <div style={{ width: '50%', minHeight: '320px' }}>
+      <VideoPlayer url="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" onVideoDimensions={data => console.log(data)} />
+    </div>
+  ).add('with YouTube video', () => <div style={{ width: '50%', minHeight: '320px' }}>
+      <VideoPlayer url="https://www.youtube.com/watch?v=qEBtvP7P5GE" onVideoDimensions={data => console.log(data)} />
     </div>
   )
-  .add('with vendor link', () => <div style={{ width: '50%' }}>
-      <VideoPlayer url="https://vimeo.com/243556536" />
+  .add('with Vimeo video', () => <div style={{ width: '50%', minHeight: '320px' }}>
+      <VideoPlayer url="http://vimeo.com/315931008" onVideoDimensions={data => console.log(data)} />
     </div>
   )
   ;
