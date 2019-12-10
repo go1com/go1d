19780180fa67d1a7c1ef715636e76c19ff0a5857
Franc <franc.hakani@go1.com>
2019-12-10 16:22:31 +0800

    add mobile icon

---
+++ src/components/Icons/Mobile.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+const Mobile: React.SFC<React.SVGProps<SVGSVGElement>> = (
+  props: React.SVGProps<SVGSVGElement>
+) => (
+  <svg fill="currentColor" viewBox="0 0 16 16" {...props}>
+    <path
+      fillRule="evenodd"
+      d="M4.667 2.083a.583.583 0 0 0-.583.584v10.666c0 .322.26.584.583.584h6.667a.583.583 0 0 0 .583-.584V2.667a.583.583 0 0 0-.583-.584H4.667zm-2.083.584c0-1.15.932-2.084 2.083-2.084h6.667c1.15 0 2.083.933 2.083 2.084v10.666c0 1.15-.933 2.084-2.083 2.084H4.667a2.083 2.083 0 0 1-2.083-2.084V2.667z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M7.25 12a.75.75 0 0 1 .75-.75h.007a.75.75 0 0 1 0 1.5H8a.75.75 0 0 1-.75-.75z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+Mobile.displayName = "IconMobile";
+
+export default Mobile;
