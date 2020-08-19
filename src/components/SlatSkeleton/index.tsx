6217e62482e05b461b2c55416968a6ccd49ddab0
Thoi Quach <thoi.quach@go1.com>
2020-08-19 18:42:03 +0700

    GO1P-35748: modify course slat for mobile

--- src/components/SlatSkeleton/index.tsx
+++ src/components/SlatSkeleton/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { breakpoints } from "../../foundations";
 import Skeleton from "../Skeleton";
 import View from "../View";
 
@@ -12,8 +13,9 @@ export default () => (
     backgroundColor="background"
     width="100%"
     overflow="hidden"
+    css={{ [breakpoints.sm]: { flexDirection: "column" } }}
   >
-    <Skeleton padding={3} height={[130, 142, 142]} width={[130, 221, 221]} />
+    <Skeleton padding={3} height={[130, 142, 142]} width={["100%", 221, 221]} />
     <View padding={5} flexGrow={1}>
       <Skeleton fontSize={3} maxWidth={450} width="70%" marginBottom={4} />
       <Skeleton
@@ -22,6 +24,7 @@ export default () => (
         maxWidth={250}
         width="50%"
         marginBottom={3}
+        css={{ [breakpoints.sm]: { display: "none" } }}
       />
       <Skeleton
         backgroundColor="faded"
