59edf16278eab38dc64f4b1de6024b07e3e945b8
Di <diana.macdonald@go1.com>
2020-11-05 13:50:48 +1100

    GO1P-38122 Add visually hidden component and…
    … update loading spinner to use visually hidden

---
+++ src/components/VisuallyHidden/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Theme from "../Theme";
+import View, { ViewProps } from "../View";
+
+export interface VisuallyHiddenProps {
+  children?: React.ReactNode;
+}
+
+const VisuallyHidden: React.SFC<VisuallyHiddenProps> = ({
+  children = "Loading",
+}: VisuallyHiddenProps) => (
+  <View
+    css={{
+      position: "absolute",
+      height: "1px",
+      width: "1px",
+      overflow: "hidden",
+      clip: "rect(1px, 1px, 1px, 1px)",
+    }}
+  >
+    {children}
+  </View>
+);
+
+VisuallyHidden.displayName = "VisuallyHidden";
+
+export default VisuallyHidden;
