efa30e349679673659f676dbf343375b83008204
Di <diana.macdonald@go1.com>
2021-01-20 11:44:58 +1000

    Disable Happo in DateRange.story.js

--- stories/DateRange.story.js
+++ stories/DateRange.story.js
@@ -1,33 +1,33 @@
 import React from "react";
 import moment from "moment";
 import { DateRange } from "../src";
-import { isHappoRun } from "happo-plugin-storybook/register";
 
-// Skipp DateRange picker for Happo since we are not able to achieve consistent screenshots for it for following reasons:
-// 1. A Focus into the date range, causes a blinking cursor
-// 2. The Picker displays the current month, which can't be influenced in the current implementation
-if (!isHappoRun()) {
-  export default {
-    title: "Original Go1d/DateRange",
-  };
+export default {
+  title: "Original Go1d/DateRange",
+  parameters: {
+    // Skip DateRange picker for Happo since we are not able to achieve consistent screenshots for it for following reasons:
+    // 1. A Focus into the date range, causes a blinking cursor
+    // 2. The Picker displays the current month, which can't be influenced in the current implementation
+    happo: false, // this will disable all `Original Go1d/DateRange` stories
+  },
+};
 
-  export const BasicDatepicker = () => <DateRange />;
+export const BasicDatepicker = () => <DateRange />;
 
-  export const DefaultValue = () => (
-    <DateRange
-      defaultValue={{
-        startDate: new moment("2019-12-05"),
-        endDate: new moment("2019-12-15"),
-      }}
-    />
-  );
+export const DefaultValue = () => (
+  <DateRange
+    defaultValue={{
+      startDate: new moment("2019-12-05"),
+      endDate: new moment("2019-12-15"),
+    }}
+  />
+);
 
-  export const ControlledValue = () => (
-    <DateRange
-      value={{
-        startDate: new moment("2019-12-05"),
-        endDate: new moment("2019-12-15"),
-      }}
-    />
-  );
-}
+export const ControlledValue = () => (
+  <DateRange
+    value={{
+      startDate: new moment("2019-12-05"),
+      endDate: new moment("2019-12-15"),
+    }}
+  />
+);
