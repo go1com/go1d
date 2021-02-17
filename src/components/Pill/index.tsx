8e87a56fc6712018015c487d614d168c3b4b04b6
Di <diana.macdonald@go1.com>
2021-02-17 15:39:17 +1000

    Improve corner radius on Pill

--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -23,7 +23,7 @@ const Pill: React.SFC<PillProps> = ({
       display="inline-flex"
       backgroundColor={color}
       backgroundOpacity={color !== "faded" && "pill"}
-      borderRadius={2}
+      borderRadius={1}
       paddingX={3}
       paddingY={2}
       {...props}
