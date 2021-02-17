c9d3c4054a85e61204ba2e152d7b031e9cb901ac
Di <diana.macdonald@go1.com>
2021-02-17 15:46:21 +1000

    Improve corner radius on SlatSkeleton

--- src/components/SlatSkeleton/index.tsx
+++ src/components/SlatSkeleton/index.tsx
@@ -15,8 +15,15 @@ export default () => (
   >
     <Skeleton padding={3} height={[130, 142, 142]} width={["100%", 221, 221]} />
     <View padding={5} flexGrow={1}>
-      <Skeleton fontSize={3} maxWidth={450} width="70%" marginBottom={4} />
       <Skeleton
+        borderRadius={1}
+        fontSize={3}
+        maxWidth={450}
+        width="70%"
+        marginBottom={4}
+      />
+      <Skeleton
+        borderRadius={1}
         backgroundColor="faded"
         fontSize={1}
         maxWidth={250}
@@ -25,6 +32,7 @@ export default () => (
         display={["none", "flex"]}
       />
       <Skeleton
+        borderRadius={1}
         backgroundColor="faded"
         fontSize={1}
         maxWidth={215}
