244386278bd938dffcaab3fa64d9c83ac1291771
Di <diana.macdonald@go1.com>
2021-03-10 10:59:53 +1000

    Indent story code

--- stories/InputGroup.story.js
+++ stories/InputGroup.story.js
@@ -35,40 +35,40 @@ export const Base = args => (
 export const WithSelectDropdownAndTextInput = args => (
   <Theme.Consumer>
     {({ radius }) => (
-  <InputGroup>
-    <SelectDropdown
-      options={[
-        { value: "chocolate", label: "Chocolate" },
-        { value: "stawberry", label: "Strawberry" },
-        { value: "vanilla", label: "Vanilla" },
-      ]}
-      optionRenderer={({ value, label }) => <View>{label}</View>}
-      onChange={(value) => window.alert(`You selected ${value}`)}
-    >
-      {({ ref, getToggleButtonProps }) => (
-        <ButtonFilled
-          {...getToggleButtonProps()}
-          innerRef={ref}
-          paddingX={3}
-          fontWeight="semibold"
-          flexGrow={1}
-          css={{
-            borderRadius: `${radius[3]}px 0px 0px ${radius[3]}px`,
-            flexGrow: 1,
-            flexShrink: 1,
-            ":hover, :focus, :active": {
-              transform: "none",
-            },
-          }}
+      <InputGroup>
+        <SelectDropdown
+          options={[
+            { value: "chocolate", label: "Chocolate" },
+            { value: "stawberry", label: "Strawberry" },
+            { value: "vanilla", label: "Vanilla" },
+          ]}
+          optionRenderer={({ value, label }) => <View>{label}</View>}
+          onChange={(value) => window.alert(`You selected ${value}`)}
         >
-          <View flexDirection="row" alignItems="center">
-            Button <IconChevronDown marginLeft={3} />
-          </View>
-        </ButtonFilled>
-      )}
-    </SelectDropdown>
-    <TextInput borderRadius={0} />
-  </InputGroup>
+          {({ ref, getToggleButtonProps }) => (
+            <ButtonFilled
+              {...getToggleButtonProps()}
+              innerRef={ref}
+              paddingX={3}
+              fontWeight="semibold"
+              flexGrow={1}
+              css={{
+                borderRadius: `${radius[3]}px 0px 0px ${radius[3]}px`,
+                flexGrow: 1,
+                flexShrink: 1,
+                ":hover, :focus, :active": {
+                  transform: "none",
+                },
+              }}
+            >
+              <View flexDirection="row" alignItems="center">
+                Button <IconChevronDown marginLeft={3} />
+              </View>
+            </ButtonFilled>
+          )}
+        </SelectDropdown>
+        <TextInput borderRadius={0} />
+      </InputGroup>
     )}
   </Theme.Consumer>
 );
