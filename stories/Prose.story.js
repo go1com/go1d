b6212e8d9bf9e2ed0a2e13f75dda709bb708d945
Di <diana.macdonald@go1.com>
2021-04-08 14:32:58 +1000

    Improve Prose story defaultValues, args, etc.

--- stories/Prose.story.js
+++ stories/Prose.story.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { Prose } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
 
-let argTypes = hideInheritedProps({}, Prose);
+const defaultValue = "<h2>Heading 2</h2><b>This is bold text</b><br /><img src='https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?h=100' alt='Here be image' title='Here be image' />"
 
 export default {
-  argTypes: argTypes,
   title: "Athletic/Prose",
   component: Prose,
+  argTypes: {
+    HTML: {
+      defaultValue: defaultValue,
+    },
+    expandable: { table: { disable: true } }
+  },
 };
 
 export const PlainHtml = args => (
-  <Prose {...args} HTML="<h2>Heading 2</h2><b>This is bold text</b><br /><img src='https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?h=100' alt='Here be image' title='Here be image' />" />
+  <Prose HTML={defaultValue} {...args} />
 );
+PlainHtml.storyName = "Plain containing HTML";
 
-PlainHtml.storyName = "Plain HTML";
-
-export const Javascript = args => (
+export const Javascript = () => (
   <Prose HTML="<script>document.write('Hello World!');</script> Script tags wont show up" />
 );
-export const Link = args => (
+Javascript.storyName = "Prose containing JavaScript";
+
+export const Link = () => (
   <Prose HTML="Hello from the world of <a href='#Test' target='_blank' norel='badtag'>Hello World!</a>" />
 );
+Link.storyName = "Prose containing a link";
 
 export const Expandable = () => (
   <Prose lineClamp={3} expandable HTML="“The <strong>empathetic explorer</strong> thinking habit can help you explore the experiences people have with compliance in their daily work. You can also hear first hand about their attitudes towards compliance. This approach can help you uncover what is important to individuals in their work and how to make compliance programs more engaging and memorable.” — Michelle Ockers" />
