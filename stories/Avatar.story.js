e16f501abac5f76164a0c25154355d89e841fd91
Di <diana.macdonald@go1.com>
2021-04-08 10:27:02 +1000

    Add multiple users icon to Avatar story

--- stories/Avatar.story.js
+++ stories/Avatar.story.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Avatar } from "../src";
 import hideInheritedProps from '../.storybook/hideInheritedProps.js'
+import IconMultipleUsers from "../src/components/Icons/MultipleUsers";
 
 let argTypes = hideInheritedProps({}, Avatar);
+const IconElement = IconMultipleUsers;
 
 export default {
   title: "Athletic/Avatar",
@@ -13,21 +15,20 @@ export default {
 export const AvatarWithAnImage = args => (
   <Avatar {...args} src="https://i.imgur.com/Ee55uvc.jpg" fullName="Leslie Knope" />
 );
-
 AvatarWithAnImage.storyName = "Avatar with an image";
 
 export const AvatarWithoutAnImage = args => <Avatar fullName="Leslie Knope" />;
-
 AvatarWithoutAnImage.storyName = "Avatar without an image";
 
 export const AvatarWithoutAnImageAndName = args => <Avatar />;
+AvatarWithoutAnImage.storyName = "Avatar without an image";
 
-AvatarWithoutAnImageAndName.storyName = "Avatar without an image and name";
+export const AvatarWithMultipleUsersIcon = args => <Avatar icon={IconElement} />;
+AvatarWithMultipleUsersIcon.storyName = "Avatar with multiple users icon";
 
 export const AvatarWithADifferentSize = args => (
   <Avatar fullName="Willy Wonka" size={4} />
 );
-
 AvatarWithADifferentSize.storyName = "Avatar with a different size";
 
 export const AvatarWithDifferentColors = args => (
@@ -38,5 +39,4 @@ export const AvatarWithDifferentColors = args => (
     size={4}
   />
 );
-
 AvatarWithDifferentColors.storyName = "Avatar with different colors";
