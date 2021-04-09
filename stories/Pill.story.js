5aa787a095c946ec945c318359ada771543d66fc
Di <diana.macdonald@go1.com>
2021-04-09 12:47:37 +1000

    Improve Pill story arg table usability
    - Use "medium" as default value to show our preference without
    necessarily making a breaking change in the component
    - Disable controls on status color-specific stories

--- stories/Pill.story.js
+++ stories/Pill.story.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Pill, View, Text } from '../src';
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, Pill);
 
 export default {
   title: 'Athletic/Pill',
   component: Pill,
   subcomponents: { View, Text },
-  parameters: {},
-  argTypes: argTypes
+  argTypes: {
+    fontWeight: {
+      defaultValue: 'medium',
+      options: ["light", "normal", "medium", "semibold", "bold"],
+    }
+  },
 };
 
-export const Default = args => <Pill {...args}>Default</Pill>;
-export const Accent = args => <Pill {...args} color='accent'>Accent</Pill>;
-export const Success = args => <Pill {...args} color='success'>Success</Pill>;
-export const Note = args => <Pill {...args} color='note'>Note</Pill>;
-export const Warning = args => <Pill {...args} color='warning'>Warning</Pill>;
-export const Danger = args => <Pill {...args} color='danger'>Danger</Pill>;
+export const Main = args => <Pill {...args}>Default</Pill>;
+
+export const Accent = () => <Pill fontWeight="medium" color='accent'>Accent</Pill>;
+
+export const Success = () => <Pill fontWeight="medium" color='success'>Success</Pill>;
+
+export const Note = () => <Pill fontWeight="medium" color='note'>Note</Pill>;
+
+export const Warning = () => <Pill fontWeight="medium" color='warning'>Warning</Pill>;
+
+export const Danger = () => <Pill fontWeight="medium" color='danger'>Danger</Pill>;
 
