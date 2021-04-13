7e56896aafb37ff2c17d7f079e8b4b21e94631f4
Di <diana.macdonald@go1.com>
2021-04-13 11:49:06 +1000

    Remove hideInheritedProps from EmptyState story

--- stories/EmptyState.story.js
+++ stories/EmptyState.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { EmptyState } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, EmptyState);
 
 export default {
   title: "Original Go1d/EmptyState",
-  argTypes: argTypes,
   component: EmptyState,
 };
 
