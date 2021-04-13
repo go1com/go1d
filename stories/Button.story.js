dd564c7a6592206a0d575fae0c62d9756df7bd1c
Di <diana.macdonald@go1.com>
2021-04-13 14:54:37 +1000

    Improve Button story usability
    - Disable unuseful controls
    - Reorder controls

--- stories/Button.story.js
+++ stories/Button.story.js
@@ -4,6 +4,19 @@ import { Button, SubmitButton } from "../src";
 export default {
   title: "Athletic/Buttons/Base",
   component: Button,
+  argTypes: {
+    backgroundColor: { defaultValue: 'background' }, // only here to reorder before disabled controls
+    color: {}, // only here to reorder before disabled controls
+    fontWeight: {}, // only here to reorder before disabled controls
+    href: { defaultValue: '#root' }, // only here to reorder before disabled controls
+    round: {}, // only here to reorder before disabled controls
+    size: {}, // only here to reorder before disabled controls
+    spinnerIcon: {}, // only here to reorder before disabled controls
+    iconColor: { control: { disable: true } },
+    sizeStyles: { control: { disable: true } },
+    iconName: { control: { disable: true } },
+    icon: { control: { disable: true } },
+  },
   parameters: {
     docs: {
       description: {
@@ -19,8 +32,9 @@ export const UnstyledButtons = args => (
   </React.Fragment>
 );
 
-export const SubmitButtonUsedInAForm = args => (
+export const SubmitButtonUsedInAForm = () => (
   <SubmitButton>Create</SubmitButton>
 );
-
 SubmitButtonUsedInAForm.storyName = "Submit Button used in a Form";
+SubmitButtonUsedInAForm.parameters = { controls: { hideNoControlsWarning: true } }
+
