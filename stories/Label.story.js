248066ca59981fe9312f4be0d2d03a5b37a753a4
Di <diana.macdonald@go1.com>
2021-04-13 14:29:53 +1000

    Improve Label story usability
    - Disable some arg types controls that don't work
    - Add story name to LabelStory
    - Use args in LabelWithStatus story

--- stories/Label.story.js
+++ stories/Label.story.js
@@ -4,14 +4,20 @@ import { Label } from "../src";
 export default {
   title: "Athletic/Label",
   component: Label,
+  argTypes: {
+    htmlFor: { control: { disable: true } },
+    statusIcon: { control: { disable: true } },
+    spinnerIcon: { description: 'Only shows when used with `statusText` or `labelSuffix`' },
+    labelSuffix: { control: { disable: true } },
+  }
 };
 
-export const _Label = args => <Label {...args}>Portal Name</Label>;
+export const LabelStory = args => <Label {...args}>Portal Name</Label>;
+LabelStory.storyName = "Label";
 
 export const LabelWithStatus = args => (
-  <Label {...args} statusText="unavailable" statusColor="danger">
+  <Label statusText="unavailable" statusColor="danger" {...args}>
     Portal Name
   </Label>
 );
-
 LabelWithStatus.storyName = "Label with status";
