c0c6b70201aee93b4755ce06520246ab9fbb594b
Di <diana.macdonald@go1.com>
2021-04-14 11:14:06 +1000

    Remove hideInheritedProps from Spinner

--- stories/Spinner.story.js
+++ stories/Spinner.story.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Spinner } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, Spinner);
-
-let customArgTypes = {
-  size: {
-    name: 'size',
-    defaultValue: '2',
-    control: { type: 'range', min: 1, max: 10 } },
-};
-
-argTypes = Object.assign(argTypes, customArgTypes);
 
 export default {
   title: "Athletic/Spinner",
-  argTypes: argTypes,
+  argTypes: {
+    size: {
+      defaultValue: '2',
+      control: { type: 'range', min: 1, max: 10 }
+    },
+  },
   component: Spinner,
 };
 
