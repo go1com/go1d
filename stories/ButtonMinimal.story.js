b0b2378869cf57d90efcb994edf0da80e1c9f455
Di <diana.macdonald@go1.com>
2021-04-14 10:21:59 +1000

    Improve ButtonMinimal story usability
    - Add Button subcomponents
    - Move colors to defaultValue field

--- stories/ButtonMinimal.story.js
+++ stories/ButtonMinimal.story.js
@@ -1,42 +1,47 @@
 import React from "react";
-import { ButtonMinimal } from "../src";
+import { ButtonMinimal, Button } from "../src";
 import { IconPlus } from "../src/components/Icons";
 
 export default {
   title: "Athletic/Buttons/ButtonMinimal",
   component: ButtonMinimal,
+  subcomponents: { Button },
 };
 
 export const MinimalButtonSubtleColour = args => (
   <ButtonMinimal {...args}>Minimal button</ButtonMinimal>
 );
-
 MinimalButtonSubtleColour.storyName = "Minimal button subtle colour";
 
 export const MinimalButtonSubtleColourIconAndLabel = args => (
-  <ButtonMinimal {...args} icon={IconPlus}>Minimal button</ButtonMinimal>
+  <ButtonMinimal icon={IconPlus} {...args}>Minimal button</ButtonMinimal>
 );
-
 MinimalButtonSubtleColourIconAndLabel.storyName = "Minimal button subtle colour (Icon and label)";
 
 export const MinimalButtonInAccentColourIconOnly = args => (
-  <ButtonMinimal {...args} color="accent" icon={IconPlus} />
+  <ButtonMinimal icon={IconPlus} {...args} />
 );
-
 MinimalButtonInAccentColourIconOnly.storyName = "Minimal button in accent colour (Icon only)";
+MinimalButtonInAccentColourIconOnly.argTypes = {
+  color: { defaultValue: 'accent' },
+};
 
 export const MinimalButtonInAccentColourIconAndLabel = args => (
-  <ButtonMinimal {...args} color="accent" icon={IconPlus}>
+  <ButtonMinimal icon={IconPlus} {...args}>
     {" "}
     Call to action
   </ButtonMinimal>
 );
-
 MinimalButtonInAccentColourIconAndLabel.storyName = "Minimal button in accent colour (Icon and label)";
+MinimalButtonInAccentColourIconAndLabel.argTypes = {
+  color: { defaultValue: 'accent' },
+};
 
 export const MinimalButtonInDangerColour = args => (
-  <ButtonMinimal {...args} color="danger">Really</ButtonMinimal>
+  <ButtonMinimal {...args}>Really</ButtonMinimal>
 );
-
 MinimalButtonInDangerColour.storyName = "Minimal button in danger colour";
+MinimalButtonInDangerColour.argTypes = {
+  color: { defaultValue: 'danger' },
+};
 
