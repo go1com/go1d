18f0e6c95267c59cdc58fd8880e6ab555dfaadd9
Di <diana.macdonald@go1.com>
2021-04-14 11:11:40 +1000

    Remove hideInheritedProps from DataTableSelectable

--- stories/DataTableSelectable.story.js
+++ stories/DataTableSelectable.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { DataTableSelectable, TR, TD, TH } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, DataTableSelectable);
 
 export default {
   title: "Original Go1d/DataTableSelectable",
-  argTypes: argTypes,
   component: DataTableSelectable,
 };
 
