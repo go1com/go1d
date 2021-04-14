3ae91dbb383cf599fcfe8c89e7dcc8e90c7e6b92
Di <diana.macdonald@go1.com>
2021-04-14 16:53:10 +1000

    Add description to InputSuffix story

--- stories/InputSuffix.story.js
+++ stories/InputSuffix.story.js
@@ -4,6 +4,13 @@ import { InputSuffix } from "../src";
 export default {
   title: "Original Go1d/InputSuffix",
   component: InputSuffix,
+  parameters: {
+    docs: {
+      description: {
+        component: 'While this component will let you use a suffix, you probably want the InputGroup component instead.'
+      }
+    },
+  },
 };
 
 export const BasicInputSuffix = args => (
