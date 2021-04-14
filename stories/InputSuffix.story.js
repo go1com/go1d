a0e9784c00275372ec69c5edc1cb3b2a454448a9
Di <diana.macdonald@go1.com>
2021-04-14 16:53:04 +1000

    Remove hideInheritedProps from InputSuffix story

--- stories/InputSuffix.story.js
+++ stories/InputSuffix.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { InputSuffix } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, InputSuffix);
 
 export default {
   title: "Original Go1d/InputSuffix",
-  argTypes: argTypes,
   component: InputSuffix,
 };
 
