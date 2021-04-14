7859a840f3f71a1167493c52aed1323a34f22524
Di <diana.macdonald@go1.com>
2021-04-14 10:22:49 +1000

    Remove hideInheritedProps from PageBody story

--- stories/PageBody.story.js
+++ stories/PageBody.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { PageBody, View, Text } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, PageBody);
 
 export default {
   title: "Original Go1d/PageBody",
-  argTypes: argTypes,
   component: PageBody,
 };
 
