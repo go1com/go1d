462a3e3f3657bb98f31577876763d8a8bac18cba
Di <diana.macdonald@go1.com>
2021-04-14 16:56:54 +1000

    Remove hideInheritedProps from PasswordInput

--- stories/PasswordInput.story.js
+++ stories/PasswordInput.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { PasswordInput } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, PasswordInput);
 
 export default {
   title: "Athletic/PasswordInput",
-  argTypes: argTypes,
   component: PasswordInput,
 };
 
