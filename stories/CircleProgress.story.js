60cea12ef3c59a9e945d786e2bb464df8a85cecf
Di <diana.macdonald@go1.com>
2021-04-14 16:58:17 +1000

    Remove hideInheritedProps from CircleProgress

--- stories/CircleProgress.story.js
+++ stories/CircleProgress.story.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { CircleProgress } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, CircleProgress);
 
 export default {
   title: "Original Go1d/CircleProgress",
-  argTypes: argTypes,
   component: CircleProgress,
 };
 
 export const CircleProgressStory = args => (
   <CircleProgress {...args} />
 );
-
 CircleProgressStory.storyName = "Circle Progress";
 
 export const WithPercentage = args => (
   <CircleProgress {...args} progress={50} size={100} lineWidth={50} showProgress={true} />
 );
-
 WithPercentage.storyName = "with percentage";
 
 export const WithNoPercentage = args => (
   <CircleProgress {...args} progress={75} size={20} lineWidth={50} showProgress={false} />
 );
-
 WithNoPercentage.storyName = "with no percentage";
+
