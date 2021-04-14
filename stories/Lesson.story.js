2bc4deabeffcd14349f41b57ef72031f3279b337
Di <diana.macdonald@go1.com>
2021-04-14 14:11:58 +1000

    Remove hideInheritedProps from Lesson story

--- stories/Lesson.story.js
+++ stories/Lesson.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Lesson } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, Lesson);
 
 export default {
   title: "Original Go1d/Lesson",
-  argTypes: argTypes,
   component: Lesson,
 };
 
