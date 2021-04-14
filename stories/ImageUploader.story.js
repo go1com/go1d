7fce590b81ac12f5a424dcff653bd8250dfa8fdf
Di <diana.macdonald@go1.com>
2021-04-14 11:20:19 +1000

    Remove hideInheritedProps from ImageUploader story

--- stories/ImageUploader.story.js
+++ stories/ImageUploader.story.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { ImageUploader, Form, Field, ButtonFilled } from "../src";
-import hideInheritedProps from '../.storybook/hideInheritedProps.js'
-
-let argTypes = hideInheritedProps({}, ImageUploader);
 
 export default {
   title: "Original Go1d/ImageUploader",
-  argTypes: argTypes,
   component: ImageUploader,
 };
 
