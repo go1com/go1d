bcf882cd2e8cd916d093bf9e537763b39a9a42b5
Di <diana.macdonald@go1.com>
2021-05-04 11:36:42 +1000

    Update duplicate IDs in DatePicker story

--- stories/DatePicker.story.js
+++ stories/DatePicker.story.js
@@ -32,9 +32,9 @@ export const Sizes = () => (
   <React.Fragment>
     <DatePicker id="id4" size="sm" defaultValue={new Date("2019-06-07T00:00:00.000+08:00")} />
     <br />
-    <DatePicker id="id4" size="md" defaultValue={new Date("2019-06-07T00:00:00.000+08:00")} />
+    <DatePicker id="id5" size="md" defaultValue={new Date("2019-06-07T00:00:00.000+08:00")} />
     <br />
-    <DatePicker id="id4" size="lg" defaultValue={new Date("2019-06-07T00:00:00.000+08:00")} />
+    <DatePicker id="id6" size="lg" defaultValue={new Date("2019-06-07T00:00:00.000+08:00")} />
   </React.Fragment>
 );
 Sizes.parameters = { controls: { hideNoControlsWarning: true } };
@@ -42,7 +42,7 @@ Sizes.parameters = { controls: { hideNoControlsWarning: true } };
 export const BasicDatepickerWithTime = args => (<DatePicker {...args} />);
 BasicDatepickerWithTime.storyName = "Basic Datepicker with Time";
 BasicDatepickerWithTime.argTypes = {
-  id: { defaultValue: "id5" },
+  id: { defaultValue: "id7" },
   time: { defaultValue: true },
   defaultValue: { defaultValue: new Date("2019-06-07 10:00") },
 };
