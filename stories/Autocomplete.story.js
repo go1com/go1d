06c6b1eca46f861444a0046fa73ea9c6bc0b8635
Di <diana.macdonald@go1.com>
2021-06-10 14:46:19 +1000

    Update Autocomplete story, component description
    … from docs/components/autocomplete.md

--- stories/Autocomplete.story.js
+++ stories/Autocomplete.story.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Autocomplete } from "../src";
+import View from "../src/components/View/";
+import Text from "../src/components/Text/";
+import Avatar from "../src/components/Avatar/";
 
 export default {
   title: "Athletic/Autocomplete",
@@ -9,7 +12,7 @@ export default {
   ],
 };
 
-export const Base = args => (
+export const BaseAutocomplete = args => (
   <Autocomplete
     lookupMethod={() => {
       // pass a method to request options
@@ -42,4 +45,104 @@ export const Base = args => (
     {...args}
   />
 );
+BaseAutocomplete.storyName = "Base";
 
+export const WithOptionCustomRenderer = args => (
+  <Autocomplete
+    lookupMethod={() => {
+      // pass a method to request options
+      const options = [
+       {
+        label: "foo1",
+        value: "foo1",
+        description: "9 members ⋅ 1 learning",
+        image: "https://i.imgur.com/Ee55uvc.jpg",
+       },
+       {
+        label: "foo2",
+        value: "foo2",
+        description: "1 members ⋅ 10 learning",
+        image: "https://i.imgur.com/Ee55uvc.jpg",
+       },
+      ];
+
+      // modify implementation state options
+      // eg. this.setState({ options });
+    }}
+    // this would come from the implementation-level state
+    // eg. this.state.options
+    options={[
+      {
+        label: "foo1",
+        value: "foo1",
+        description: "9 members ⋅ 1 learning",
+        image: "https://i.imgur.com/Ee55uvc.jpg",
+      },
+      {
+        label: "foo2",
+        value: "foo2",
+        description: "1 members ⋅ 10 learning",
+        image: "https://i.imgur.com/Ee55uvc.jpg",
+      },
+    ]}
+    onSelectOption={() => {
+      console.log("this method fires when user makes a selection");
+    }}
+    dropdownProps={{
+      maxWidth: "100%",
+      width: "100%",
+    }}
+ optionRenderer={item => {
+  const { label, displayProps: { description, image } } = item;
+  return (
+   <View
+    display="flex"
+    flexDirection="row"
+    alignItems="center"
+   >
+    <View marginRight={3}>
+     <Avatar src={image} fullName="Leslie Knope" size={4} />
+    </View>
+    <View>
+     <View marginBottom={0}>
+      <Text
+       fontWeight="semibold"
+       color="default"
+       lineHeight="display"
+       fontSize={3}
+      >
+       {label}
+      </Text>
+     </View>
+     {description && (
+      <View>
+       <Text color="subtle" lineHeight="display">
+        {description}
+       </Text>
+      </View>
+     )}
+    </View>
+   </View>
+  );
+ }}
+ optionFormatter={option => {
+  return {
+    label: option.label,
+    value: option.value,
+    displayProps: {
+      image: option.image,
+      description: option.description,
+    },
+   };
+  }
+ }
+    inputProps={{
+      fontSize: 1,
+    }}
+    labelProps={{
+      fontSize: 1,
+    }}
+    flexGrow={1}
+    {...args}
+  />
+);
