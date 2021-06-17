7d51c78646418f6b161431e4fd7daab4ff1c0df7
Di <diana.macdonald@go1.com>
2021-06-17 16:15:37 +1000

    Update Prose story
    … from docs/components/prose.md

--- stories/Prose.story.js
+++ stories/Prose.story.js
@@ -33,14 +33,35 @@ export const Javascript = () => (
   <Prose HTML="<script>document.write('Hello World!');</script> Script tags wont show up" />
 );
 Javascript.storyName = "Prose containing JavaScript";
+Javascript.parameters = {
+  docs: {
+    description: {
+      story: 'Does not allow banned tags such as `script`',
+    },
+  },
+};
 
 export const Link = () => (
   <Prose HTML="Hello from the world of <a href='#Test' target='_blank' norel='badtag'>Hello World!</a>" />
 );
 Link.storyName = "Prose containing a link";
+Link.parameters = {
+  docs: {
+    description: {
+      story: 'Does not allow banned attributes such as `norel`',
+    },
+  },
+};
 
 export const Expandable = () => (
   <Prose lineClamp={3} expandable HTML="“The <strong>empathetic explorer</strong> thinking habit can help you explore the experiences people have with compliance in their daily work. You can also hear first hand about their attitudes towards compliance. This approach can help you uncover what is important to individuals in their work and how to make compliance programs more engaging and memorable.” — Michelle Ockers" />
 );
 Expandable.storyName = "Expandable prose";
+Expandable.parameters = {
+  docs: {
+    description: {
+      story: 'Line clamp with show more/less button',
+    },
+  },
+};
 
