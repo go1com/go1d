4f02ded1ed74dc282c1ac2caee61d4de2c86cbec
Di <diana.macdonald@go1.com>
2021-06-22 15:37:44 +1000

    Center docs-site layout

--- docs-site/components/Layout.js
+++ docs-site/components/Layout.js
@@ -1,13 +1,22 @@
 import * as React from 'react'
 // import Head from 'next/head'
 import View from '@go1d/go1d/build/components/View'
+import { foundations } from '@go1d/go1d';
 
 // const Layout: React.FC = ({ children }) => {
 const Layout = ({ children }) => {
 
   return (
     <React.Fragment>
-      <View padding={5}>
+      <View
+        marginX="auto"
+        padding={5}
+        css={{
+          [foundations.breakpoints.sm]: { maxWidth: "375px" },
+          [foundations.breakpoints.md]: { maxWidth: "972px" },
+          [foundations.breakpoints.lg]: { maxWidth: "1068px" },
+        }}
+      >
         {children}
       </View>
     </React.Fragment>
