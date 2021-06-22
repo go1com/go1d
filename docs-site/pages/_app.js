c2ce547b299ab03956f614682b3c356a4e278136
Di <diana.macdonald@go1.com>
2021-06-22 11:43:13 +1000

    Add site-docs Head

--- docs-site/pages/_app.js
+++ docs-site/pages/_app.js
@@ -1,5 +1,6 @@
 import Provider from "@go1d/go1d/build/components/Provider";
 import globalCSS from '@go1d/go1d/build/foundations/globalCSS';
+import Head from 'next/head'
 import Layout from '../components/Layout'
 
 globalCSS();
@@ -7,6 +8,10 @@ globalCSS();
 function DocsSite({ Component, pageProps }) {
   return (
     <Provider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Go1d docs-site</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
