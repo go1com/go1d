943dd981bdeb80c5ebef0b23b93dab1408cce272
Di <diana.macdonald@go1.com>
2021-07-02 12:21:44 +1000

    Add Go1d logo to docs-site and update overview

--- docs-site/pages/_app.js
+++ docs-site/pages/_app.js
@@ -19,6 +19,29 @@ function DocsSite({ Component, pageProps }) {
         <title>Go1d docs-site</title>
       </Head>
       <Layout>
+        <header>
+          <Link href="https://go1d.go1.com/">
+            <Text
+              color="accent"
+              element="p"
+              marginTop={5}
+              marginBottom={2}
+              fontFamily="sansSerif"
+              fontSize={2}
+              fontWeight="medium"
+              lineHeight="display"
+              display="flex"
+              css={{alignItems: "center"}}
+            >
+              <img
+                src="https://cdn.go1static.com/assets/go1d-static-assets/images/Go1d%20logo.png"
+                alt="Go1d logo"
+                style={{width: "1em", height: "1em", marginRight: "0.5em"}}
+              />
+              Go1d docs-site
+            </Text>
+          </Link>
+        </header>
         <MDXProvider components={mdxComponents}>
           <main>
             <Component {...pageProps} />
