7323f713941c1f25d0e48f481941af10581caccd
Di <diana.macdonald@go1.com>
2021-08-06 10:10:35 +1000

    Update GlobalMessage story
    - adds editable message to story controls
    - adds browser warning example
    - adds default color

--- stories/GlobalMessage.story.js
+++ stories/GlobalMessage.story.js
@@ -16,23 +16,27 @@ export default {
   }
 };
 
-export const Basic = args => (
-  <GlobalMessage {...args} close={() => alert("close event")}>
-    <Text>
-      This is a message{" "}
-      <Text color="accent">
-        <Link
-          css={{
-            display: "inline",
-            textDecoration: "underline",
-          }}
-          href="https://foo.com"
-        >
-          read more
-        </Link>
-      </Text>
-      .
+const richContentExample = (
+  <Text>
+    This is a global message{" "}
+    <Text color="accent">
+      <Link
+        css={{
+          display: "inline",
+          textDecoration: "underline",
+        }}
+        href="https://foo.com"
+      >
+        read more
+      </Link>
     </Text>
+    .
+  </Text>
+);
+
+export const Basic = ({ storyExampleText, ...args }) => (
+  <GlobalMessage color="default" {...args} close={() => alert("close event")}>
+    {storyExampleText}
   </GlobalMessage>
 );
 Basic.storyName = "Base";
@@ -45,10 +49,24 @@ Basic.argTypes = {
     },
     options: ["success", "warning", "danger", "note"],
   },
+  storyExampleText: {
+    defaultValue: richContentExample,
+    name: "example message in story",
+    control: { type: "text" }
+  }
 };
 
+export const WarningExample = () => (
+  <GlobalMessage color="default" status="warning" close={() => alert("close event")}>
+    <Text>
+      We no longer support Internet Explorer. For the best experience, please use a modern internet browser, such as Google Chrome.
+    </Text>
+  </GlobalMessage>
+);
+WarningExample.parameters = { controls: { hideNoControlsWarning: true } };
+
 export const LongText = () => (
-  <GlobalMessage status="success" close={() => alert("close event")}>
+  <GlobalMessage color="default" status="success" close={() => alert("close event")}>
     <Text>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy text {" "}
       <Text color="accent">
